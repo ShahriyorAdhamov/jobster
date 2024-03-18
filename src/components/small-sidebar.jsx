c98f7fe84198e73fa6753ec55818035d02de1dba
Shahriyor <shahriyoradhamov@icloud.com>
2024-03-18 23:19:51 +0500

    add sidebar functionality

--- src/components/small-sidebar.jsx
+++ src/components/small-sidebar.jsx
@@ -1,7 +1,36 @@
+import { FaTimes } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { toggleSidebar } from '../features/user/user-slice';
+import Logo from './logo';
+import NavLinks from './nav-links';
 
 const SmallSidebar = () => {
-	return <Wrapper>SmallSidebar</Wrapper>;
+	const { isSidebarOpen } = useSelector(store => store.user);
+	const dispatch = useDispatch();
+
+	const toggle = () => {
+		dispatch(toggleSidebar());
+	};
+	return (
+		<Wrapper>
+			<div
+				className={
+					isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+				}
+			>
+				<div className='content'>
+					<button className='close-btn' onClick={toggle}>
+						<FaTimes />
+					</button>
+					<header>
+						<Logo />
+					</header>
+					<NavLinks toggleSidebar={toggle} />
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default SmallSidebar;
