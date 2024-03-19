bcb6953ae92211e83b0e020d9b2d699045bd4bca
Shahriyor <shahriyoradhamov@icloud.com>
2024-03-19 22:28:14 +0500

    prepare add job starter

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import BigSidebar from './big-sidebar';
 import FormRow from './form-row';
+import FormRowSelect from './form-row-select';
 import Logo from './logo';
 import Navbar from './navbar';
 import SmallSidebar from './small-sidebar';
 
-export { BigSidebar, FormRow, Logo, Navbar, SmallSidebar };
+export { BigSidebar, FormRow, FormRowSelect, Logo, Navbar, SmallSidebar };
--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import jobSlice from './features/job/job-slice';
 import userSlice from './features/user/user-slice';
 export const store = configureStore({
 	reducer: {
 		user: userSlice,
+		job: jobSlice,
 	},
 });
